aa70903b71c55eea134f3015dc06a79845530f20
Vini <ViniciusVask@users.noreply.github.com>
2017-07-31 03:36:45 -0300

    redirect to the index

--- postar-contato.php
+++ postar-contato.php
@@ -1,7 +1,14 @@
 <?php
-    include_once("conexao-contato.php");
+    include("conexao-contato.php");
 ?>
 
-<?php 
-    include("index.php");
+
+<?php
+ #abaixo, criamos uma variavel que terá como conteúdo o endereço para onde haverá o redirecionamento:  
+ $redirect = "/";
+ 
+ #abaixo, chamamos a função header() com o atributo location: apontando para a variavel $redirect, que por 
+ #sua vez aponta para o endereço de onde ocorrerá o redirecionamento
+ header("location:$redirect");
+ 
 ?>
