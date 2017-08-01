1169b42b439f4ac9fb45d7aeb42dd8642c81cdde
Vini <ViniciusVask@users.noreply.github.com>
2017-08-01 01:50:42 -0300

    corrigindo mais um erro do merge

--- index.php
+++ index.php
@@ -123,7 +123,6 @@
                          <img src="./img/ivan_amaro.jpg" class="img-circle" alt="Cinque Terre" width="259" height="236"> 
                         <h3>Ivan Amaro</h3>
                         <p class="text-muted">Entusiasta de IoT e cryptocurrencies, presta serviços de integração para ATM's e automação de operações. Atualmente integra o time de operações da <a href="https://foxbit.exchange/" style="font-weight: bold" target="_blank">Foxbit</a></p>
-                        <p class="text-muted">Fundador da <a href="https://econobit.com.br/" target="_blank">Econobit</a>, que oferece soluções para comerciantes e consumidores tornarem suas transações financeiras com a moeda digital Bitcoin mais simples e seguras.</p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 text-center">
