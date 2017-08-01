89a30b66e46ad37b11db44e6eb560cee400ea322
Vini <ViniciusVask@users.noreply.github.com>
2017-08-01 01:36:30 -0300

    testing redirect to https 4

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ function redirectTohttps() {
 
 if($_SERVER[‘HTTPS’]!=”on”) {
 
-$redirect= "https://".$_SERVER[‘HTTP_HOST’].$_SERVER[‘REQUEST_URI’];
+$redirect= "https://".$_SERVER[‘REQUEST_URI’];
 
 header(“Location:$redirect”); } }
 
