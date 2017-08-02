c40a39f5e8155e3f45116d897f0ae44fd7692716
Vini <ViniciusVask@users.noreply.github.com>
2017-08-02 11:41:13 -0300

    adding OG types

--- index.php
+++ index.php
@@ -36,6 +36,17 @@
     <!-- Theme CSS -->
     <link href="css/creative.min.css" rel="stylesheet">
     <link href="css/creative.css" rel="stylesheet">
+  
+  <!-- OG TAGS -->
+    <meta property="og:url" content="https://cripto.cash" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Bitcoin Meetup São Paulo | HOJE 02 de Agosto" />
+    <meta property="og:description" content="Entusiastas e especialistas se reúnem para debates e palestras sobre o futuro do bitcoin pós 1º de Agosto e o que isso significa para o investidor." />
+    <meta property="og:image" content="img/cutmypic.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="302" />
+    <meta property="og:image:height" content="170" />
+
 
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
